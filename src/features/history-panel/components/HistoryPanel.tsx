aec0acff1ded5ed5b3744abd2394b177181bead2
meme-d0ge <MEME_D0GE@proton.me>
2026-03-07 19:14:36 +0300

    feat(HistoryPanel): implement Ctrl+Z, Ctrl+Shift+Z for undo/redo

--- src/features/history-panel/components/HistoryPanel.tsx
+++ src/features/history-panel/components/HistoryPanel.tsx
@@ -1,5 +1,6 @@
 import { Redo, Undo } from 'lucide-react';
 import { observer } from 'mobx-react-lite';
+import { useCallback, useEffect } from 'react';
 import { useStore } from '@/app/providers/StoreProvider.tsx';
 import { Button } from '@/shared/components/ui/button.tsx';
 
@@ -7,6 +8,26 @@ export const HistoryPanel = observer(
 	({ className }: { className?: string }) => {
 		const { entities } = useStore();
 		const { undo, redo, canRedo, canUndo } = entities.elementsStore;
+
+		const keyDownHandle = useCallback(
+			(event: KeyboardEvent) => {
+				if (event.ctrlKey && event.code === 'KeyZ') {
+					if (event.shiftKey) {
+						redo();
+					} else {
+						undo();
+					}
+				}
+			},
+			[undo, redo],
+		);
+
+		useEffect(() => {
+			document.addEventListener('keydown', keyDownHandle);
+			return () => {
+				document.removeEventListener('keydown', keyDownHandle);
+			};
+		}, [keyDownHandle]);
 
 		return (
 			<div className={`flex flex-row gap-2 ${className}`}>
